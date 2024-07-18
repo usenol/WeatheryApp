392a90cb949b919e25141544d3484e6033a2b153
Umut Şenol <umut_senol@outlook.com>
2024-07-18 17:35:21 +0300

    Vector Status

--- Others/AppDelegate.swift
+++ Others/AppDelegate.swift
@@ -10,9 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
     
-    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = WeatherViewController()
+         
+        return true
+    }
     
 }
-
--- WeatheryApp/WeatherViewController.swift
+++ WeatheryApp/WeatherViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    let backgroundView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        style()
+        layout()
+    }
 
 }
 
+extension WeatherViewController {
+    
+    func style() {
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        if let image = UIImage(named: "day-background") {
+            backgroundView.image = image
+        } else {
+            print("Resim yüklenemedi!")
+        }
+        backgroundView.contentMode = .scaleAspectFill
+    }
+    
+    func layout() {
+        view.addSubview(backgroundView)
+        
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
