8d87181565ecad3b617e8d3679e5708951dde057
Umut Şenol <umut_senol@outlook.com>
2024-07-18 17:31:22 +0300

    Vector Assets

---
+++ Others/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  WeatheryApp
+//
+//  Created by Umut Şenol on 18.07.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    
+    
+    
+}
+
