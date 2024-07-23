494fde2da5a237777dd4111fa791beb6c7c47b9a
Umut Şenol <umut_senol@outlook.com>
2024-07-23 14:39:49 +0300

    Stack Views Added

--- WeatheryApp/WeatherViewController.swift
+++ WeatheryApp/WeatherViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 class WeatherViewController: UIViewController {
 
     let backgroundView = UIImageView()
+    let rootStackView = UIStackView()
+    
+    //Search
+    let searchStackView = UIStackView()
     let locationButton = UIButton()
     let searchButton = UIButton()
     let searchTextField = UITextField()
+    
+    //Weather
+    let conditionImageView = UIImageView()
+    let temperatureLabel = UILabel()
+    let cityLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +44,16 @@ extension WeatherViewController {
             print("Image failed to load!")
         }
         backgroundView.contentMode = .scaleAspectFill
+        
+        ///Root Stack View
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.alignment = .trailing
+        rootStackView.spacing = 10
+        
+        ///Search Stack View
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.spacing = 8
         
         ///Location Button Style
         locationButton.translatesAutoresizingMaskIntoConstraints = false
@@ -61,37 +80,53 @@ extension WeatherViewController {
         searchTextField.borderStyle = .roundedRect
         searchTextField.backgroundColor = .systemFill
         
+        //Weather
+        conditionImageView.translatesAutoresizingMaskIntoConstraints = false
+        conditionImageView.image = UIImage(systemName: "sun.max")
+        conditionImageView.tintColor = .label
+        
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.font = UIFont.boldSystemFont(ofSize: 80)
+        temperatureLabel.text = "38°C"
+         
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        cityLabel.text = "Ankara"
     }
     
     func layout() {
         view.addSubview(backgroundView)
-        view.addSubview(locationButton)
-        view.addSubview(searchButton)
-        view.addSubview(searchTextField)
+        view.addSubview(rootStackView)
+        
+        rootStackView.addArrangedSubview(searchStackView)
+        rootStackView.addArrangedSubview(conditionImageView)
+        rootStackView.addArrangedSubview(temperatureLabel)
+        rootStackView.addArrangedSubview(cityLabel)
+        
+        searchStackView.addArrangedSubview(locationButton)
+        searchStackView.addArrangedSubview(searchTextField)
+        searchStackView.addArrangedSubview(searchButton)
         
         NSLayoutConstraint.activate([
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    
+            rootStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: rootStackView.trailingAnchor, multiplier: 1),
             
-            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            searchStackView.widthAnchor.constraint(equalTo: rootStackView.widthAnchor),
+            
             locationButton.widthAnchor.constraint(equalToConstant: 40),
             locationButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            //view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
             searchButton.widthAnchor.constraint(equalToConstant: 40),
             searchButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
-            searchTextField.trailingAnchor.constraint(equalTo: searchButton.leadingAnchor, constant: -8),
-            //searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
-            searchTextField.widthAnchor.constraint(equalToConstant: 40),
-            searchTextField.heightAnchor.constraint(equalToConstant: 40),
+            conditionImageView.heightAnchor.constraint(equalToConstant: 120),
+            conditionImageView.widthAnchor.constraint(equalToConstant: 120),
         ])
     }
 }
